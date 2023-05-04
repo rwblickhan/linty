1da18d8b42590964320f8800bfd11fa75d9c7910
Russell Blickhan <rwblickhan@gmail.com>
2023-05-03 18:44:41 -0700

    Partition warnings and errors

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
+use anyhow::Ok;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "snake_case")]
 enum Severity {
     Warning,
     Error,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Rule {
     id: String,
     message: String,
@@ -20,16 +21,39 @@ struct Rule {
     excludes: Option<Vec<String>>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Config {
     rules: Vec<Rule>,
 }
 
+struct Violation {
+    rule: Rule,
+}
+
 fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
     let file = File::open(Path::new(".lintyconfig.json"))?;
     let reader = BufReader::new(file);
 
     let config: Config = serde_json::from_reader(reader)?;
+    println!("{:?}", config);
+
+    let violations: Vec<Violation> = config
+        .rules
+        .into_iter()
+        .flat_map(|rule| apply_rule(rule).unwrap())
+        .collect();
+
+    let (warnings, errors): (Vec<Violation>, Vec<Violation>) =
+        violations
+            .into_iter()
+            .partition(|violation| match &violation.rule.severity {
+                Severity::Warning => true,
+                Severity::Error => false,
+            });
+
     Ok(())
+}
+
+fn apply_rule(rule: Rule) -> anyhow::Result<Vec<Violation>> {
+    Ok(vec![Violation { rule: rule }])
 }
